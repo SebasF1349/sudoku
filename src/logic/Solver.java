d2d2d1f2e6b58df46ef04d7918ed008543b02bef
SebasF <33849340+SebasF1349@users.noreply.github.com>
2024-06-19 18:11:49 -0300

    skeleton

---
+++ src/logic/Solver.java
@@ -0,0 +1,23 @@
+package logic;
+
+public class Solver {
+    public boolean numberIsValid(int[][] board, int number, int row, int col) {
+        return true;
+    }
+
+    public boolean checkRow(int[][] board, int number, int row) {
+        return true;
+    }
+
+    public boolean checkCol(int[][] board, int number, int col) {
+        return true;
+    }
+
+    public boolean checkSquare(int[][] board, int number, int row, int col) {
+        return true;
+    }
+
+    public boolean solve(int[][] board) {
+       return true;
+    }
+}
