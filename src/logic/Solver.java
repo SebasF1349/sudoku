cb5537989db037e0824a529adea766f0f1a62edb
SebasF1349 <ratablanca711@gmail.com>
2024-06-26 19:50:55 -0300

    feat: Solver.solve()

--- src/logic/Solver.java
+++ src/logic/Solver.java
@@ -3,29 +3,46 @@ package logic;
 import model.Sudoku;
 
 public class Solver {
-    private Sudoku sudoku;
+  private final Sudoku sudoku;
 
-    public Solver(Sudoku sudoku) {
-        this.sudoku = sudoku;
-    }
+  public Solver(Sudoku sudoku) {
+    this.sudoku = sudoku;
+  }
 
-    public boolean numberIsValid(int number, int row, int col) {
-        return true;
-    }
+  public boolean numberIsValid(int number, int row, int col) {
+    return checkRow(number, row) && checkCol(number, col) && checkSquare(number, row, col);
+  }
 
-    public boolean checkRow(int number, int row) {
-        return true;
-    }
+  public boolean checkRow(int number, int row) {
+    return true;
+  }
 
-    public boolean checkCol(int number, int col) {
-        return true;
-    }
+  public boolean checkCol(int number, int col) {
+    return true;
+  }
 
-    public boolean checkSquare(int number, int row, int col) {
-        return true;
-    }
+  public boolean checkSquare(int number, int row, int col) {
+    return true;
+  }
 
-    public boolean solve() {
-        return true;
+  public boolean solve() {
+    for (int row = 0; row < sudoku.getNumberNodes(); row++) {
+      for (int col = 0; col < sudoku.getNumberNodes(); col++) {
+        if (!sudoku.hasEdge(row, col)) {
+          for (int possibleNumber = 0;
+              possibleNumber <= sudoku.getNumberNodes();
+              possibleNumber++) {
+            if (numberIsValid(possibleNumber, row, col)) {
+              sudoku.addEdge(col, row, possibleNumber);
+              if (solve()) {
+                return true;
+              }
+              sudoku.removeEdge(row, col);
+            }
+          }
+        }
+      }
     }
+    return false;
+  }
 }
--- src/model/Sudoku.java
+++ src/model/Sudoku.java
@@ -2,39 +2,42 @@ package model;
 
 public class Sudoku {
 
-    private int[][] board;
-    final private int NODES = 9;
+  private int[][] board;
+  private final int NODES = 9;
 
-    public Sudoku() {
-        board = new int[NODES][NODES];
+  public Sudoku() {
+    board = new int[NODES][NODES];
+  }
+
+  public int getNumberNodes() {
+    return NODES;
+  }
+
+  public void addEdge(int col, int row, int value) {
+    board[col][row] = value;
+  }
+
+  public void removeEdge(int row, int col) {
+    board[col][row] = 0;
+  }
+
+  public boolean hasEdge(int row, int col) {
+    return board[col][row] != 0;
+  }
+
+  public int getValue(int row, int col) {
+    return board[col][row];
+  }
+
+  public int[] getEdgesFrom(int row) {
+    return board[row];
+  }
+
+  public int[] getEdgesTo(int col) {
+    int[] ret = new int[NODES];
+    for (int i = 0; i < NODES; i++) {
+      ret[i] = board[i][col];
     }
-
-    public void addEdge(int col, int row, int value) {
-        board[col][row] = value;
-    }
-
-    public void removeEdge(int row, int col) {
-        board[col][row] = 0;
-    }
-
-    public boolean hasEdge(int row, int col) {
-        return board[col][row] != 0;
-    }
-
-    public int getValue(int row, int col) {
-        return board[col][row];
-    }
-
-    public int[] getEdgesFrom(int row) {
-        return board[row];
-    }
-
-    public int[] getEdgesTo(int col) {
-        int[] ret = new int[NODES];
-        for (int i = 0; i < NODES; i++) {
-           ret[i] = board[i][col];
-        }
-        return ret;
-    }
-
+    return ret;
+  }
 }
