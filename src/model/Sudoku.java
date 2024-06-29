8752fdbd669e8dec94d02d669323b04e52df7ee4
SebasF1349 <ratablanca711@gmail.com>
2024-06-29 16:34:28 -0300

    feat: add isValid to Sudoku

--- src/model/Sudoku.java
+++ src/model/Sudoku.java
@@ -1,39 +1,70 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Sudoku {
 
-  private int[][] board;
-  private final int NODES = 9;
+    private int[][] board;
+    private final int NODES = 9;
 
-  public Sudoku() {
-    board = new int[NODES][NODES];
-  }
+    public Sudoku() {
+        board = new int[NODES][NODES];
+    }
 
-  public void addBoard(int[][] board) {
-    this.board = board;
-  }
+    public void addBoard(int[][] board) {
+        this.board = board;
+    }
 
-  public int[][] getBoard() {
-    return board;
-  }
+    public int[][] getBoard() {
+        return board;
+    }
 
-  public int getNumberNodes() {
-    return NODES;
-  }
+    public int getNumberNodes() {
+        return NODES;
+    }
 
-  public void addEdge(int row, int col, int value) {
-    board[row][col] = value;
-  }
+    public void addEdge(int row, int col, int value) {
+        board[row][col] = value;
+    }
 
-  public void removeEdge(int row, int col) {
-    board[row][col] = 0;
-  }
+    public void removeEdge(int row, int col) {
+        board[row][col] = 0;
+    }
 
-  public boolean hasEdge(int row, int col) {
-    return board[row][col] != 0;
-  }
+    public boolean hasEdge(int row, int col) {
+        return board[row][col] != 0;
+    }
 
-  public int getValue(int row, int col) {
-    return board[row][col];
-  }
+    public int getValue(int row, int col) {
+        return board[row][col];
+    }
+
+    public boolean isValid() {
+        for (int i = 0; i < NODES; i++) {
+            Set<Integer> rowValues = new HashSet<>();
+            Set<Integer> colValues = new HashSet<>();
+            Set<Integer> squareValues = new HashSet<>();
+            for (int j = 0; j < NODES; j++) {
+                if (board[i][j] != 0 && !rowValues.add(board[i][j])) {
+                    return false;
+                }
+                if (board[j][i] != 0 && !colValues.add(board[j][i])) {
+                    return false;
+                }
+            }
+            int startRow = i - (i % 3);
+            int startCol = i % 3 * 3;
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    int value = board[row + startRow][col + startCol];
+                    if (value != 0 && !squareValues.add(value)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
 }
